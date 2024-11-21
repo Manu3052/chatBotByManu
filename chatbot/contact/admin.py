34b22fbb05fdb664d099239abfae16826c12bfa4
Manuele M <manuele.mauricio.l@gmail.com>
2024-11-21 00:35:33 -0300

    feat: criando app contact

---
+++ chatbot/contact/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Contact
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Contact model.
+    Displays 'id', 'name', 'email', 'cpf', and 'telephone' in the list view.
+    Allows searching by 'name', 'email', 'cpf', and 'telephone'.
+    """
+    list_display = ('id', 'name', 'email', 'cpf', 'telephone')
+    search_fields = ('name', 'email', 'cpf', 'telephone')
---
+++ chatbot/contact/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Contact(models.Model):
+    """
+    Model to store contact information.
+    Fields:
+        - id: Unique identifier for each contact (Primary Key).
+        - cpf: CPF of the contact (optional).
+        - telephone: Telephone number of the contact (optional).
+        - email: Unique and non-null email address of the contact.
+        - name: Full name of the contact (non-null).
+    """
+    id = models.AutoField(primary_key=True)
+    cpf = models.CharField(max_length=11, blank=True, null=True)
+    telephone = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        """
+        Return the full name of the contact as a string.
+        """
+        return self.name
